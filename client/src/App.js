28514be8ed871069c4746c7043615325eeccfe8b
Saba Zikaria <szikaria961@gmail.com>
2023-02-12 10:34:40 -0500

    Test api fetching using useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data);
+      }
+    )
+  }, [])
   return (
     <div className="App">Receipt Generator</div>
   );
