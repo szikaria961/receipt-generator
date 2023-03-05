e8a3c6e8ecdb02ea73fc8a968f7c43a0ea30831b
Saba Zikaria <szikaria961@gmail.com>
2023-03-04 20:56:42 -0500

    Create a skeleton UI of what receipt generator is going to look like

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,44 @@
-import { useEffect, useState } from 'react';
-import './App.css';
 
 const App = () => {
-  const [backendData, setBackendData] = useState([{}]);
-
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data);
-      }
-    )
-  }, [])
+  const handlPrint = () => {
+    window.print();
+  }
   return (
-    <div className="App">Receipt Generator</div>
+    <>
+      <main className="m-5 p-5 lg:max-w-xl lg:mx-auto bg-white rounded shadow">
+        <header className="flex flex-col items-center jutify-center mb-5">
+          <div className="font-bold text-4xl mb-3">Receipt Generator</div>
+          <div>
+            <ul className="flex items-center justify-between flex-wrap">
+              <li>
+                <button className="btn btn-print p-2" onClick={handlPrint}>Print</button>
+              </li>
+              <li>
+                <button className="btn btn-download p-2">Download</button>
+              </li>
+            </ul>
+          </div>
+        </header>
+        {/* Customers Info*/}
+        <section className="flex flex-col items-end justify-end">
+            <h2>Customers Name</h2>
+            <p>Order Number</p>
+            <p>Date Order Placed</p>
+        </section>
+        {/* Product Info will store in some table*/}
+        <section>
+            <h2>Product Name</h2>
+            <p>Quantity</p>
+        </section>
+        <footer>
+          <ul>
+            <li>Business Name</li>
+            <li>Business email</li>
+            <li>Social Media Handle</li>
+          </ul>
+        </footer>
+      </main>
+    </>
   );
 }
 
